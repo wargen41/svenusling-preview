9e92e0bf15688f13f03993ae4e4410d4e584a6d1
wargen41 <106013425+wargen41@users.noreply.github.com>
2026-05-06 18:35:26 +0200

    Config

--- public/m/index.php
+++ public/m/index.php
@@ -1,13 +1,13 @@
 <?php
-require __DIR__ . '/../config.php';
+require __DIR__ . '/../../config.php';
 require __DIR__ . '/../api-requests.php';
 
 $id = $_GET['id'];
 $sanitized = sanitizeQuery($id);
 if($sanitized === $id){
-    $location = API_BASE_URL.'/movie.php?id='.$id;
+    $location = SITE_BASE_URL.'/movie.php?id='.$id;
 }else{
-    $location = API_BASE_URL.'/search-movies.php?query='.rawurlencode($id);
+    $location = SITE_BASE_URL.'/search-movies.php?query='.rawurlencode($id);
 }
 
 header("Location: {$location}");
--- public/p/index.php
+++ public/p/index.php
@@ -1,13 +1,13 @@
 <?php
-require __DIR__ . '/../config.php';
+require __DIR__ . '/../../config.php';
 require __DIR__ . '/../api-requests.php';
 
 $id = $_GET['id'];
 $sanitized = sanitizeQuery($id);
 if($sanitized === $id){
-    $location = API_BASE_URL.'/person.php?id='.$id;
+    $location = SITE_BASE_URL.'/person.php?id='.$id;
 }else{
-    $location = API_BASE_URL.'/search-persons.php?query='.rawurlencode($id);
+    $location = SITE_BASE_URL.'/search-persons.php?query='.rawurlencode($id);
 }
 
 header("Location: {$location}");
